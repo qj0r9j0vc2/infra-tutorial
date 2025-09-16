df558893cd6d4bdf913d3a209a82806c6d1d1185
qj0r9j0vc2 <me@qj0r9j0vc2.me>
2025-09-16 15:26:32 +0900

    update

---
+++ terraform/main.tf
@@ -0,0 +1,23 @@
+# providers.tf
+terraform {
+  required_providers { aws = { source = "hashicorp/aws" version = "~> 5.0" } }
+}
+provider "aws" { region = var.region }
+
+# variables.tf
+variable "region" { type = string; default = "ap-northeast-2" }
+variable "key_name" { type = string }
+variable "instance_type" { type = string; default = "t3.small" }
+
+# main.tf
+resource "aws_instance" "demo" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = var.instance_type
+  key_name      = var.key_name
+  tags          = { Name = "tf-demo" }
+}
+data "aws_ami" "ubuntu" {
+  most_recent = true
+  owners      = ["099720109477"] # Canonical
+  filter { name = "name" values = ["ubuntu/images/hvm-ssd/ubuntu-*-22.04-amd64-server-*"] }
+}
